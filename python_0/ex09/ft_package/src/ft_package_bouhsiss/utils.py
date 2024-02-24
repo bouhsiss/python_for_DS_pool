c3ce0b5c83ef2d106c1532b1a604e87e4598fde3
houda bouhsiss <houda.bouhsiss-ext@um6p.ma>
2024-02-24 10:13:51 +0100

    added conda env

--- python_0/ex09/ft_package/src/ft_package_bouhsiss/utils.py
+++ python_0/ex09/ft_package/src/ft_package_bouhsiss/utils.py
@@ -1,2 +1,12 @@
 def count_in_list(lst, item):
+    """
+    Counts the number of occurrences of an item in a list.
+
+    Args:
+        lst (list): The list to search in.
+        item: The item to count.
+
+    Returns:
+        int: The number of occurrences of the item in the list.
+    """
     return lst.count(item)
