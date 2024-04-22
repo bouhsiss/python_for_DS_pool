713822e0666ec7bce5279784648294c564bb198f
bouhsiss <hbouhsis@student.1337.ma>
2024-04-22 18:31:19 +0100

    modified the docstring and added the source distirbution and build distrubtion

--- python_0/ex06/ft_filter.py
+++ python_0/ex06/ft_filter.py
@@ -3,13 +3,10 @@ def function_to_apply(x):
 
 
 def ft_filter(function_to_apply, list_of_inputs):
-    """
-    ft_filter(function or None, iterable) --> filter object
+    """filter(function or None, iterable) --> filter object
 
-    Return an iterator yielding those items of iterable for \
-          which function(item)
-    is true. If function is None, return the items that are true.
-    """
+Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true."""
     if function_to_apply:
         return [i for i in list_of_inputs if function_to_apply(i)]
     return [i for i in list_of_inputs if i]
