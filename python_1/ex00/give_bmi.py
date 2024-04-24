7fe5b2d072001261501794562a348aee2cbe0aca
houda bouhsiss <houda.bouhsiss-ext@um6p.ma>
2024-04-24 18:03:46 +0100

    starting pyhton_01

--- python_1/ex00/give_bmi.py
+++ python_1/ex00/give_bmi.py
@@ -1,1 +1,22 @@
+import numpy as np
 
+def give_bmi(height : list[int | float], weight: list[int | float]) -> list[int | float]:
+    """
+    This function takes in a list of heights and weights and returns a list of BMI values.
+    
+    Parameters:
+    height : list(int | float) : A list of heights in cm.
+    weight : list(int | float) : A list of weights in kg.
+    
+    Returns:
+    list(int | float) : A list of BMI values.
+    """
+    try :
+        height = np.array(height)
+        weight = np.array(weight)
+        
+        bmi = weight / (height / 100) ** 2
+    except Exception as e:
+        print("Error : " + e)
+        bmi = None
+    return bmi
