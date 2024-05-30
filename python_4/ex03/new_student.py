7bb71b4b9d87c533af496ec493e065e0c9707789
bouhsiss <hbouhsis@student.1337.ma>
2024-05-30 14:38:48 +0100

    PYTHON4 : ex03 done

---
+++ python_4/ex03/new_student.py
@@ -0,0 +1,22 @@
+import random
+import string
+from dataclasses import dataclass, field
+
+
+def generate_id() -> str:
+    return "".join(random.choices(string.ascii_lowercase, k=15))
+
+
+@dataclass
+class Student:
+    """Class to represent a student"""
+    name: str
+    surname: str
+    active: bool = field(default=True, init=False)
+    login: str = field(init=False)
+    id: str = field(default_factory=generate_id, init=False)
+
+    def __post_init__(self):
+        self.login = self.name[0].upper() + self.surname.lower()
+
+
