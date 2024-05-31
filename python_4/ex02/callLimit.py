d776afa752fdcb220bd3dce659e548123bfb9a28
bouhsiss <hbouhsis@student.1337.ma>
2024-05-31 15:51:23 +0100

    PYTHON4 : better docstrings

--- python_4/ex02/callLimit.py
+++ python_4/ex02/callLimit.py
@@ -1,14 +1,42 @@
 def callLimit(limit: int):
-    """a function that takes as argument a call limit of another function \
-and blocks its execution above a limit"""
+    """Creates a decorator that limits the number of times a function can be \
+called
+
+    Parameters:
+    limit: int : the number of times the function can be called
+
+    Returns:
+    function : A decorator function that wraps the target function and \
+enforces the call limit
+    """
     count = 0
 
     def callLimiter(function):
-        """a function that takes as argument a function and blocks its \
-execution above a limit"""
+        """Wraps the given function to enforce a call limit
+
+        Parameters:
+        function : function : the function to be wrapped and limited
+
+        Returns:
+        function: A wrapped version of the input function that will only \
+exceute up to the limit.
+        """
         def limit_function(*args, **kwargs):
-            """a function that takes as argument a function and blocks its \
-execution above a limit"""
+            """
+            Executes the wrapped function if the call limit has not been \
+reached.
+
+            Parameters:
+            args : tuple : the positional arguments to be passed to the \
+wrapped function
+            kwargs : dict : the keyword arguments to be passed to the wrapped \
+function
+
+            Returns:
+            The result of the wrapped function if the call limit has not been \
+reached.
+            Prints an error message if the call limit has been reached.
+            """
             nonlocal count
             if count < limit:
                 count += 1
